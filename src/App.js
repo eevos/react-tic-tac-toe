862acbdabc947f18f991cc47e0c8f584bc57e2d1
edwin <e.e.vos@hotmail.com>
2023-03-28 21:11:00 +0200

    2nd try for board

--- src/App.js
+++ src/App.js
@@ -1,32 +1,28 @@
 import React from 'react'
+import ReactDOM from "react-dom/client";
 
 export default function Square() {
 
-  return <MyGrid />;
+  return (
+    <div>
+      <Row />
+      <Row />
+      <Row />
+    </div>
+  );
 }
 
-function MyGrid() {
-  let grid = [];
-  let row = [];
-  let number = 0;
-  console.log('mygrid');
+function Row() {
 
-  for (let y = 0; y < 3; y++) {
-    console.log('loop y');
-
-    number++;
-    for (let x = 0; x < 3; x++) {
-      console.log('loop x');
-
-      row[x] = <MySquare value={number} />
-      number++;
-    }
-    grid[y] = row;
-  }
-
-  return <MySquare value={number} />;
+  return (
+    <div className="board-Row">
+      <MySquare />
+      <MySquare />
+      <MySquare />
+    </div>
+  );
 }
 
-function MySquare({value}) {
+function MySquare({ value }) {
   return <button className="square">{value != null ? value : "_"}</button>;
 };
