a38e651d9887de93dd18ba5df3bcda9bbfe894fd
edwin <e.e.vos@hotmail.com>
2023-03-29 10:39:22 +0200

    Added handleClick for buttons in Board

--- src/App.js
+++ src/App.js
@@ -3,24 +3,33 @@ import ReactDOM from "react-dom/client";
 import { useState } from "react"
 
 export default function Board() {
+  // https://react.dev/learn/tutorial-tic-tac-toe#setup-for-the-tutorial
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i){
+    console.log(i);
+    const nextSquares = squares.slice();
+    nextSquares[i] = 'X';
+    setSquares(nextSquares);
+  }
 
   return (
     <table>
       <tbody>
         <tr className="board-Row">
-          <MySquare />
-          <MySquare />
-          <MySquare />
+          <MySquare value={squares[0]} onSquareClick={() => handleClick(0)} />
+          <MySquare value={squares[1]} onSquareClick={() => handleClick(1)}/>
+          <MySquare value={squares[2]} onSquareClick={() => handleClick(2)}/>
         </tr>
         <tr className="board-Row">
-          <MySquare />
-          <MySquare />
-          <MySquare />
+          <MySquare value={squares[3]} onSquareClick={() => handleClick(3)}/>
+          <MySquare value={squares[4]} onSquareClick={() => handleClick(4)}/>
+          <MySquare value={squares[5]} onSquareClick={() => handleClick(5)}/>
         </tr>
         <tr className="board-Row">
-          <MySquare />
-          <MySquare />
-          <MySquare />
+          <MySquare value={squares[6]} onSquareClick={() => handleClick(6)}/>
+          <MySquare value={squares[7]} onSquareClick={() => handleClick(7)}/>
+          <MySquare value={squares[8]} onSquareClick={() => handleClick(8)}/>
         </tr>
       </tbody>
     </table>
@@ -37,11 +46,12 @@ function Row() {
   );
 }
 
-function MySquare() {
-  let [value, setValue] = useState("");
+function MySquare({value, onSquareClick}) {
+  console.log(value);
 
-  function handleClick() {
-    setValue("X");
-  }
-  return <td onClick={handleClick} className="square">{value != null ? value : ""}</td>;
+  return <td
+    className="square"
+    onClick={onSquareClick}>
+    {value}
+  </td>;
 };
