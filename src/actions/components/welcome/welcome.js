d927e40b234be37c2368715f2d8dbb4a2d23c70e
edwin <e.e.vos@hotmail.com>
2023-07-02 08:35:50 +0200

    Added welcome-page

---
+++ src/actions/components/welcome/welcome.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { useState } from "react";
+
+export default function Welcome() {
+
+    return (
+        <div className="welcome">
+            <p>Welcome world!</p>
+        </div>
+    );
+}
